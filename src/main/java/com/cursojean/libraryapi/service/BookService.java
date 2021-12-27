37cc20999d85fede40b939234453382dbeefd32d
j100667 <ti.jean@unimedpg.com.br>
2021-12-27 16:38:15 -0300

    Finalizado módulo de livros API e iniciado de emprestimos (aula 58).

--- src/main/java/com/cursojean/libraryapi/service/BookService.java
+++ src/main/java/com/cursojean/libraryapi/service/BookService.java
@@ -2,6 +2,8 @@ package com.cursojean.libraryapi.service;
 
 
 import com.cursojean.libraryapi.model.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -16,4 +18,6 @@ public interface BookService {
     void delete(Book book)throws IllegalArgumentException;
 
     Book update(Book map);
+
+    Page<Book> find(Book filter, Pageable pageRequest);
 }
